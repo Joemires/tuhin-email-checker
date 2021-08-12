87153785de2f8e342d13770c2d8756aa0d60ad7a
Joseph Bassey <joemires@localhost.localdomain>
2021-08-12 06:21:23 +0100

    Modified FinderCest, removed dummy test

--- tests/FinderCest.php
+++ tests/FinderCest.php
@@ -25,22 +25,14 @@ class FinderCest
                     fwrite($file, "");
                     fclose($file);
                 } catch (Exception $e) {
-                    unlink(getenv('EXIST_PATH'));
+                    @unlink(getenv('EXIST_PATH'));
                 }
             } else {
-                $path = "EXIST";
-                unlink($path);
-                $I->amOnUrl('https://iforgot.apple.com/password/verify/appleid');
-                $I->fillField(["class" => "iforgot-apple-id"], 'joemires20@yandex.com');
-                $I->click('Continue');
-                $I->waitForJS("return $.active == 0;", 60);
-                
-                try {
-                    $I->see('password');
-                    $file = fopen($path, "w") or die("Unable to open file!");
-                    fwrite($file, "");
-                    fclose($file);
-                } catch (Exception $e) {
+                // $path = "EXIST";
+                // @unlink($path);
+                // $I->waitForJS("return $.active == 0;", 60);
+                // try {
+                // } catch (Exception $e) {
                     
                 }
             }
